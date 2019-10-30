652310a8a8f4fe6cc83b4433e069347a403d094e
T. Küchel <devel@zukuul.de>
2019-10-30 09:49:02 +0100

    add error handling, indent output

--- benchmark_webui.py
+++ benchmark_webui.py
@@ -31,33 +31,47 @@ payload = {'username': args.user, 'mode': 'normal', 'password': args.password }
 l = s.post(URLBase+"/api/core/auth", json=payload, verify=False)
 print("Login ", end='')
 if (l.status_code == requests.status_codes.codes.ok):
-    print("ok, loads in ", l.elapsed.total_seconds(), " seconds.")
-
-    n = s.get(URLBase+"/api/core/identity", verify=False)
-    print("Identity retrieval ", end='')
-    if (n.status_code == 200):
-        print("ok, loads in ", n.elapsed.total_seconds(), " seconds.")
-        if args.verbose:
-            obj = json.dumps(n.json(), indent=2)
-            if formatpretty:
-                colorful_json = highlight(obj, lexers.JsonLexer(), formatters.TerminalFormatter())
-                obj = colorful_json
-            print(obj)
+    if "success" in l.json():
+        if l.json()["success"]:
+            print(" ok    ", end='')
+        else:
+            print(" failed", end='')
     else:
-        print(" failed.")
-
-    o = s.post(URLBase+"/api/lmn/quota/", verify=False)
-    print("Quota retrieval ", end='')
-    if (o.status_code == 200):
-        print("ok, loads in ", o.elapsed.total_seconds(), " seconds.")
-        if args.verbose:
-            obj = json.dumps(o.json(), indent=2)
-            if formatpretty:
-                colorful_json = highlight(obj, lexers.JsonLexer(), formatters.TerminalFormatter())
-                obj = colorful_json
-            print(obj)
-    else:
-        print(" failed.")
-        
+        print(" returned unexpected content", end='')
 else:
-    print(" failed.")
+    print(" failed", end='')
+print(", ", l.elapsed.total_seconds(), " seconds.")
+
+n = s.get(URLBase+"/api/core/identity", verify=False)
+print("Profile", end='')
+if (n.status_code == 200):
+    if "identity" in n.json() and "profile" in n.json()["identity"]:
+        if n.json()["identity"]['profile']:
+            print(" ok   ", end='')
+        else:
+            print(" empty", end='')
+    if args.verbose:
+        obj = json.dumps(n.json(), indent=2)
+        if formatpretty:
+            colorful_json = highlight(obj, lexers.JsonLexer(), formatters.TerminalFormatter())
+            obj = colorful_json
+        print(obj)
+else:
+    print(" failed", end='')
+print(", ", n.elapsed.total_seconds(), " seconds.")
+
+
+
+o = s.post(URLBase+"/api/lmn/quota/", verify=False)
+print("Quota ", end='')
+if (o.status_code == 200):
+    print(" ok    ", end='')
+    if args.verbose:
+        obj = json.dumps(o.json(), indent=2)
+        if formatpretty:
+            colorful_json = highlight(obj, lexers.JsonLexer(), formatters.TerminalFormatter())
+            obj = colorful_json
+        print(obj)
+else:
+    print(" failed", end='')
+print(", ", o.elapsed.total_seconds(), " seconds.")
