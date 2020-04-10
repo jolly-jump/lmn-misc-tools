1732cc7f329f70666bfd109f468633719d380d08
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-04-11 01:52:23 +0200

    show offline only, when command line option --showoffline is used, use escape seq. otherwise, one line per pc

--- check_room.sh
+++ check_room.sh
@@ -9,30 +9,32 @@ room=$1
 #echo "Linux?"
 #parallel-ssh -h /etc/hosts.$room -o asdf.out -e asdf.err -P uptime >/dev/null
 
+
 while read host; 
 do
-    echo -n "$host: "
     ping -c 1 -t 2 $host 2>/dev/null >/dev/null
     if [ $? -eq 0 ]; then
 	# LINBO?
-	linbo-ssh -T -n -o "ConnectTimeout=1" $host uptime 2>/dev/null 
+	echo -n -e "\033[2K\033[100D"
+	echo -n "$host: "
+	RETlinbo=$(linbo-ssh -T -n -o "ConnectTimeout=1" $host uptime 2>/dev/null)
 	if [ $? -eq 255 ]; then
 	    # Linux?
-    	    ssh -T -n -o "ConnectTimeout=1" $host cat /.linbo 2>/dev/null
+    	    RETlinux=$(ssh -T -n -o "ConnectTimeout=1" $host cat /.linbo 2>/dev/null )
 	    if [ $? -eq 255 ]; then
-		echo " neither LINBO nor Linux, not a PC?"
+		echo "neither LINBO nor Linux, not a PC?"
 	    else
-		echo -n "Linux status: "
+		echo -n "$RETlinux, status: "
 		resp=$(ssh -T -n -o "ConnectTimeout=1" $host who)
 		if [ -z "$resp" ]; then
-		    echo "idle"
+		    echo "idle: " `ssh -T -n -o "ConnectTimeout=1" $host ps aux | sort -nrk 3,3 | head -n 1 | cut -c 1-50`
 		else
 		    echo $resp
 		fi   
 		
 	    fi
 	else
-	    echo -n "LINBO status: "
+	    echo -n "$RETlinbo, LINBO status: "
 	    resp=$(linbo-ssh -T -n -o "ConnectTimeout=1" $host 'ps aux | grep linbo_cmd | grep -v grep')
 	    if [ -z "$resp" ]; then
 		echo "idle"
@@ -46,9 +48,13 @@ do
 
 	fi
     else
-	echo "offline, maybe?"
+	if [ -n "$2" -a "$2" = "--showoffline" ];  then
+	   echo "$host: offline, maybe?"
+	else
+	    echo -n "."
+	fi
     fi
     
 done </etc/hosts.$room
 
-
+echo
