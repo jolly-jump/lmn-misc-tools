04541aece0df8b1e6c28e36625aefdaf27391ac6
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-07-05 11:32:20 +0200

    fix published, this script is obsolete from linuxmuster-webui7 1.0.147 onwards

--- workaround_enable_studentlogin.sh
+++ workaround_enable_studentlogin.sh
@@ -3,8 +3,9 @@
 # This piece of software is licensed under:
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+grep 'sophomorixRole=student' /usr/lib/linuxmuster-webui/plugins/lmn_auth/api.py >/dev/null && { echo "fix/workaround seems to be in the source, exiting."; exit 0 ;} || echo "no workaround in the source"
 sed -i "s/(sophomorixRole=schooladministrator) /(sophomorixRole=schooladministrator)(sophomorixRole=student) /" /usr/lib/linuxmuster-webui/plugins/lmn_auth/api.py
 echo -n "Check if student is in filter: "
-grep student /usr/lib/linuxmuster-webui/plugins/lmn_auth/api.py >/dev/null && echo ok
+grep student /usr/lib/linuxmuster-webui/plugins/lmn_auth/api.py >/dev/null && echo "workaround is now in the source"
 echo "Restarting Schulkonsole/WebUI"
 systemctl restart linuxmuster-webui
